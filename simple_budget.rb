cc4e9b8abaaf9f6922fd39f2f89df4d8044779d7
jm4444 <jmeredith.4444@gmail.com>
2017-10-10 20:02:23 -0500

    User ready version
    * Added improved comments that explain the syntax of the program
    * Adjusted function names to be more discernible from variable names
    * Added several functions to calculate, organize, and write the budget
    * The budget is now saved as a text file named by the user
    * Removed several lines of code from that were left over from the program's predecessor
    * Renamed variables to make more sense
    * Removed extraneous code
    
    **MISSING FROM THIS VERSION**
    * There is no error checking, so if a user types in a string where an integer is needed, the program will fail

--- simple_budget.rb
+++ simple_budget.rb
@@ -1,6 +1,21 @@
 # This program calculates what someone/s make per month, per year,
 # and what they can afford to pay monthly for a living space
 
+# All function names end with "_func" to distinguish them from variables.
+# "member" refers to a single individual involved in the budget. Each member
+# is stored as a nested array inside of the array "members"
+# "members" refers to the group of individuals as a whole. They are stored
+# in an array called "members".
+# "user" in the context of code is a variable used to reference a nested member,
+# i.e. "user = 1" would be referencing the second member of the budget.
+# "user" in the context of comments and meta is referring to the person
+# actually running the program.
+# Comments that look like "# - - - - - - TEXT - - - - - - - #" are used to
+# separate the different groupings of code blocks withing the
+# program. They are merely for the sake of easing the process
+# of reading the code.
+
+
 # This clears the terminal screen
 Gem.win_platform? ? (system "cls") : (system "clear")
 
@@ -8,27 +23,49 @@ Gem.win_platform? ? (system "cls") : (system "clear")
 
 # - - - - - - - - - - - - - - - - - FUNCTIONS - - - - - - - - - - - - - - - - - - #
 
-# This calculates a person's monthly income after tax
+# This calculates a member's monthly income after tax
 # An estimate of 10% tax is used
-def monthly_income(hourly_pay, hours_worked)
+def monthly_income_func(hourly_pay, hours_worked)
   monthly_gross_income = hourly_pay * hours_worked * 4
   monthly_net_income = monthly_gross_income - (monthly_gross_income * 0.10)
   return monthly_net_income
 end
 
-# This calculates a person's yearly income
-def annual_income(monthly_net_income)
-  yearly_income = monthly_net_income * 12
-  return yearly_income
+# This calculates a member's annual income
+def annual_income_func(monthly_net_income)
+  annual_income = monthly_net_income * 12
+  return annual_income
 end
 
-def combined_incomes(monthly_net_income, yearly_income)
-  # This will add up all the user's income
+# This calculates the combined monthly incomes of the members
+def combined_monthly_func(number_of_people, members)
+  combined_monthly = 0
+  if number_of_people > 1
+    (0...number_of_people).each do |user|
+      # This adds the previous value of the "combined_monthly" variable to the
+      # "monthly_net_income" element of each user's grouping in the array "members"
+      combined_monthly += members[user][1]
+    end
+  end
+  return combined_monthly
+end
+
+# This calculates the combined annual incomes of the members
+def combined_annual_func(number_of_people, members)
+  combined_annual = 0
+  if number_of_people > 1
+    (0...number_of_people).each do |user|
+      # This adds the previous value of the "combined_annual" variable to the
+      # "annual_income" element of each user's grouping in the array "members"s
+      combined_annual += members[user][2]
+    end
+  end
+  return combined_annual
 end
 
 # This creates an array for each member that contains their name, monthly income,
 # and annual income
-def member_register(user)
+def member_register_func(user)
   member = []
 
   print "\n\nName of budget member ##{user}: "
@@ -39,30 +76,29 @@ def member_register(user)
   hours_worked = $stdin.gets.chomp.to_i
 
   # This calculates the monthly net income
-  monthly_net_income = monthly_income(hourly_pay, hours_worked)
-  # This calculates the yearly income
-  yearly_income = annual_income(monthly_net_income)
+  monthly_net_income = monthly_income_func(hourly_pay, hours_worked)
+  # This calculates the annual income
+  annual_income = annual_income_func(monthly_net_income)
 
   # This adds each of the entered and calculated variables into an array known as
   # "member". This array is returned at the end of the function
   member << member_name
   member << monthly_net_income
-  member << yearly_income
-
+  member << annual_income
 
   # The function outputs the "member" array
   return member
 end
 
-# This loops the function "member_register" for as many members as the user
+# This loops the function "member_register_func" for as many members as the user
 # specifies
-def member_register_loop(number_of_people)
+def member_register_loop_func(number_of_people)
   # This creates an empty array called "members"
   members = []
   (1..number_of_people).each do |user|
-    # This runs the "member_register" function and stores the output array
+    # This runs the "member_register_func" function and stores the output array
     # inside the variable "member"
-    member = member_register(user)
+    member = member_register_func(user)
 
     members << member
   end
@@ -70,73 +106,110 @@ def member_register_loop(number_of_people)
   return members
 end
 
+# This prints each member's individual income at the start of the budget message
+def individual_incomes_message_func(number_of_people, members)
+  individual_incomes = []
+  (0...number_of_people).each do |user|
+    message =  "\n#{members[user][0]} makes $#{sprintf('%.2f', members[user][1])} per month, and $#{sprintf('%.2f', members[user][2])} per year."
 
-# - - - - - - - - - - - - - - - USER INPUT - - - - - - - - - - - - - - - - - - - #
+    individual_incomes << message
+  end
+  return individual_incomes
+end
+
+# This writes all of the calculations and member information to a text file
+def write_to_output_file_func(number_of_people, individual_incomes, output_budget_text_file, budget_message)
+  (0...number_of_people).each do |user|
+    output_budget_text_file.write(individual_incomes[user])
+  end
+  output_budget_text_file.write(budget_message)
+end
+
+
+
+
+
+
+# - - - - - - - - - - - - - - - RUNNING THE PROGRAM - - - - - - - - - - - - - - - #
 
 print "How many people is this budget for? "
 number_of_people = $stdin.gets.chomp.to_i
-puts "#{number_of_people}"
 
-# This runs the function "member_register_loop" for the amount of people the
+# This runs the function "member_register_loop_func" for the amount of people the
 # user specifies above and stores the output in the variable "members"
-members = member_register_loop(number_of_people)
-puts members
+members = member_register_loop_func(number_of_people)
+
+# This combines the users' monthly incomes and stores it in the variable
+# "combined_monthly"
+combined_monthly = combined_monthly_func(number_of_people, members)
+
+# This combines the users' annual incomes and stores them in the variable
+# "combined_annual"
+combined_annual = combined_annual_func(number_of_people, members)
+
+
 
 # - - - - - - - - - - - - - - - CALCULATIONS - - - - - - - - - - - - - - - - - - #
 
-# These calculate what we can afford each month; i.e., our budget
-#rent = combined_monthly * 0.3
-#bills = combined_monthly * 0.2
-#gas = combined_monthly * 0.2
-#groceries = combined_monthly * 0.18
-#monthly_leftovers = combined_monthly - (rent + bills + gas + groceries)
+# These lines calculate the various categories of the budget. They are
+# self explanatory. The percentages are averages based on research and
+# budget advising that can be found on the internet. These can be easily
+# tweaked without ramifications on the program's ability to run. While each
+# category skews towards a more conservative spending allowance,
+# the "monthly_leftovers" variable shows the user their unallocated income,
+# which can be dispersed through the other categories should the user desire.
+
+living = combined_monthly * 0.3
+bills = combined_monthly * 0.2
+gas = combined_monthly * 0.2
+groceries = combined_monthly * 0.18
+leftover = combined_monthly - (living + bills + gas + groceries)
 
 
 
 # - - - - - - - - - - - - - - - - - PRINTED MESSAGE - - - - - - - - - - - - - - - #
 
-#budget_printed = """
-#Justin makes $#{sprintf('%.2f', justins_monthly_net)} a month, and Brandi makes $#{sprintf('%.2f', brandis_monthly_net)} a month.
-#Combined, this is $#{sprintf('%.2f', combined_monthly)}.
-#Justin makes $#{sprintf('%.2f', justins_salary)} a year, and Brandi makes $#{sprintf('%.2f', brandis_salary)} a year.
-#Combined, we make $#{sprintf('%.2f', combined_salary)} a year.
+individual_incomes = individual_incomes_message_func(number_of_people, members)
 
-#Here is a break down of our monthly budget:
-#  * You can afford a living expense of around $#{sprintf('%.2f', rent)}.
-#  * You can afford around $#{sprintf('%.2f', bills)} in bills.
-#  * You can afford around $#{sprintf('%.2f', gas)} in gas.
-#  * You can afford around $#{sprintf('%.2f', groceries)} in groceries and food.
+budget_message = """
 
-#This leaves you with roughly $#{sprintf('%.2f', monthly_leftovers)} left over for spending and saving.
-#Keep in mind that these estimates are based on loose averages,
-#and provide room for flexibility.
-#Also remember that you should try to stay under budget so that
-#you have more left over each month.
-#{}"""
+Combined, you make $#{sprintf('%.2f', combined_monthly)} per month, and $#{sprintf('%.2f', combined_annual)} per year.
 
-# This prints out that information
-# I have decided to leave this part of the code out since it seems redundant to show it to the user
-# and print it to a text file. I am not removing this line completely though in case I change my mind.
-#puts "\n#{budget_printed}"
+Here is a break down of your monthly budget:
+  * You can afford a living expense of $#{sprintf('%.2f', living)}.
+  * You can afford $#{sprintf('%.2f', bills)} in bills.
+  * You can afford $#{sprintf('%.2f', gas)} in gas.
+  * You can afford $#{sprintf('%.2f', groceries)} in groceries and food.
+
+This leaves you with roughly $#{sprintf('%.2f', leftover)} left over
+for spending and saving.
+Keep in mind that these estimates are based on averages,
+and provide room for flexibility.
+Also remember to try and stay under budget so that
+you have more left over each month.
+"""
 
 
 
-# - - - - - - - - - - - - - - - - - - FILE WRITING - - - - - - - - - - - - - - - - #
+# - - - - - - - - - - - - - - - - - - FILE WRITING - - - - - - - - - - - - - - - #
 
 # This line allows the user to name their output budget file
-#print "\n\nWhat would you like to name your budget? "
-#output_file_name = $stdin.gets.chomp
+print "\n\nWhat would you like to name your budget? "
+output_file_name = $stdin.gets.chomp
 
 # This ensures the file is a text file
-#output_file_name = "#{output_file_name}.txt"
+output_file_name = "#{output_file_name}.txt"
 
-# This changes the directory to the budget folder
-# It would be good for in the future to figure out how to have it autmatically place it in the correct year folder
-# This method means each year I'll have to change this code manually
-#Dir.chdir "/users/jmere/desktop/life/finances/monthly budgets/2017"
-#output_file = open(output_file_name, 'w')
+# This creates the file with the name provided by the user and opens it in write
+# mode.
+output_budget_text_file = open(output_file_name, 'w')
 
 # This writes to the text file named by the user
-#output_file.write(budget_printed)
+write_to_output_file_func(number_of_people, individual_incomes, output_budget_text_file, budget_message)
 
-#output_file.close
+# This closes the file
+output_budget_text_file.close
+
+puts "\n\n\nYour budget can be found in the same folder as this program: \n\n#{Dir.pwd}\n\n\n\n"
+
+puts "Thank you for using Simple Budget!\n\n"
