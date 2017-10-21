4664a8a443d7603f38c816df6e009ade6ae3e668
Justin Meredith <jmeredith.4444@gmail.com>
2017-10-21 13:07:20 -0500

    * Some reformatting for ease of reading * Added an argument to the savings_goal_message_func that prints who the goal belongs to. Variables and lines were reworked with this argument, called 'savings_goal_owner' * The savings goal feature now works as intended with a single member budget, a multi member budget with a shared goal, and a multi member budget where only one member has a goal

--- simple_budget.rb
+++ simple_budget.rb
@@ -192,14 +192,14 @@ def savings_goal_func(members, number_of_people)
     savings_goal_message = individual_savings_goal_calculations_func(members, number_of_people, savings_goal)
   elsif number_of_people > 1
     puts "\n  Is this a shared goal between members or an individual goal?"
-    puts "    1) All members are working towards this goal."
-    puts "    2) This goal belongs to one member of the budget."
+    puts "    a) All members are working towards this goal."
+    puts "    b) This goal belongs to one member of the budget."
     print "\n   > "
-    user_answer = $stdin.gets.chomp.to_i
+    user_answer = $stdin.gets.chomp
 
-    if user_answer == 1
+    if user_answer == "a"
       savings_goal_message = group_savings_goal_calculations_func(members, savings_goal)
-    elsif user_answer == 2
+    elsif user_answer == "b"
       savings_goal_message = individual_savings_goal_calculations_func(members, number_of_people, savings_goal)
     end
   end
@@ -209,6 +209,8 @@ end
 # This creates a savings goal for a single member and stores it in a hash.
 def individual_savings_goal_calculations_func(members, number_of_people, savings_goal)
   if number_of_people == 1
+    savings_goal_owner = members[0][:name]
+
     # These calculate various monthly amounts the user can save from their leftovers
     # automatically to present to the user as preset options for savings and time frames.
     preset_1 = [(members[0][:leftovers] * 0.45), (savings_goal[:amount] / (members[0][:leftovers] * 0.45))]
@@ -228,13 +230,13 @@ def individual_savings_goal_calculations_func(members, number_of_people, savings
     # These take the user's answer, perform the according function, and store the
     # printed savings message in a variable.
     if user_answer == "a"
-      savings_goal_message = savings_goal_message_func(preset_1, savings_goal)
+      savings_goal_message = savings_goal_message_func(preset_1, savings_goal, savings_goal_owner)
 
     elsif user_answer == "b"
-      savings_goal_message = savings_goal_message_func(preset_2, savings_goal)
+      savings_goal_message = savings_goal_message_func(preset_2, savings_goal, savings_goal_owner)
 
     elsif user_answer == "c"
-      savings_goal_message = savings_goal_message_func(preset_3, savings_goal)
+      savings_goal_message = savings_goal_message_func(preset_3, savings_goal, savings_goal_owner)
 
     elsif user_answer == "d"
       # This while loop is here so that if the user enters an amount to save
@@ -252,18 +254,82 @@ def individual_savings_goal_calculations_func(members, number_of_people, savings
 
         else
           savings_goal_calculations = [monthly_savings, savings_goal[:amount] / monthly_savings]
-          savings_goal_message = savings_goal_message_func(savings_goal_calculations, savings_goal)
+          savings_goal_message = savings_goal_message_func(savings_goal_calculations, savings_goal, savings_goal_owner)
           break
         end
 
+      end
+    end
+
+  else
+    user_number = 1
+    puts "\n  Which budget member does this goal belong to?"
+
+    # This loop prints each members name as an option the user can choose
+    members.each do |user|
+      puts "    #{user_number}) #{user[:name]}"
+      user_number += 1
+    end
+    print "    > "
+    user_member_answer = $stdin.gets.chomp.to_i
+    user_member_answer -= 1
+    savings_goal_owner = members[user_member_answer][:name]
+
+    # These calculate various monthly amounts the user can save from their leftovers
+    # automatically to present to the user as preset options for savings and time frames.
+    preset_1 = [(members[user_member_answer][:leftovers] * 0.45), (savings_goal[:amount] / (members[user_member_answer][:leftovers] * 0.45))]
+    preset_2 = [(members[user_member_answer][:leftovers] * 0.55), (savings_goal[:amount] / (members[user_member_answer][:leftovers] * 0.55))]
+    preset_3 = [(members[user_member_answer][:leftovers] * 0.75), (savings_goal[:amount] / (members[user_member_answer][:leftovers] * 0.75))]
+
+    # This presents the above calculations to the user and asks them to choose
+    # one of the options.
+    puts "\n  How much would you like to save each month?"
+    puts "    a) Save $#{sprintf('%.2f', preset_1[0])} each month for #{sprintf('%.0f', preset_1[1])} months."
+    puts "    b) Save $#{sprintf('%.2f', preset_2[0])} each month for #{sprintf('%.0f', preset_2[1])} months."
+    puts "    c) Save $#{sprintf('%.2f', preset_3[0])} each month for #{sprintf('%.0f', preset_3[1])} months."
+    puts "    d) Set your own monthly amount."
+    print "\n   > "
+    user_answer = $stdin.gets.chomp
+
+    # These take the user's answer, perform the according function, and store the
+    # printed savings message in a variable.
+    if user_answer == "a"
+      savings_goal_message = savings_goal_message_func(preset_1, savings_goal, savings_goal_owner)
+
+    elsif user_answer == "b"
+      savings_goal_message = savings_goal_message_func(preset_2, savings_goal, savings_goal_owner)
+
+    elsif user_answer == "c"
+      savings_goal_message = savings_goal_message_func(preset_3, savings_goal, savings_goal_owner)
+
+    elsif user_answer == "d"
+      # This while loop is here so that if the user enters an amount to save
+      # monthly greater than there leftovers, they can quickly be brought back to
+      # this point.
+      while true
+        puts "\nYou have $#{sprintf('%.2f', members[user_member_answer][:leftovers])} left over each month, so don't try to save more than that each month."
+        print "\nHow much would you like to save each month? $"
+        monthly_savings = $stdin.gets.chomp.to_f
+
+        if monthly_savings > members[user_member_answer][:leftovers]
+          puts "\nYou shouldn't set a monthly saving goal that is greater than the amount you have"
+          puts "leftover at the end of each month. You can always save more than you set for"
+          puts "your monthly goal, but you should keep the goal realistic."
+
+        else
+          savings_goal_calculations = [monthly_savings, savings_goal[:amount] / monthly_savings]
+          savings_goal_message = savings_goal_message_func(savings_goal_calculations, savings_goal, savings_goal_owner)
+          break
+        end
       end
     end
   end
   savings_goal_message
 end
 
-# This creates a savings goal for the group of members and stores it in a hash
+# This creates a savings goal for the group of members
 def group_savings_goal_calculations_func(members, savings_goal)
+  savings_goal_owner = "Household"
   group_leftovers = leftovers_func(members)
   # These calculate various monthly amounts the user can save from their leftovers
   # automatically to present to the user as preset options for savings and time frames.
@@ -284,13 +350,13 @@ def group_savings_goal_calculations_func(members, savings_goal)
   # These take the user's answer, perform the according function, and store the
   # printed savings message in a variable.
   if user_answer == "a"
-    savings_goal_message = savings_goal_message_func(preset_1, savings_goal)
+    savings_goal_message = savings_goal_message_func(preset_1, savings_goal, savings_goal_owner)
 
   elsif user_answer == "b"
-    savings_goal_message = savings_goal_message_func(preset_2, savings_goal)
+    savings_goal_message = savings_goal_message_func(preset_2, savings_goal, savings_goal_owner)
 
   elsif user_answer == "c"
-    savings_goal_message = savings_goal_message_func(preset_3, savings_goal)
+    savings_goal_message = savings_goal_message_func(preset_3, savings_goal, savings_goal_owner)
 
   elsif user_answer == "d"
     # This while loop is here so that if the user enters an amount to save
@@ -308,7 +374,7 @@ def group_savings_goal_calculations_func(members, savings_goal)
 
       else
         savings_goal_calculations = [monthly_savings, savings_goal[:amount] / monthly_savings]
-        savings_goal_message = savings_goal_message_func(savings_goal_calculations, savings_goal)
+        savings_goal_message = savings_goal_message_func(savings_goal_calculations, savings_goal, savings_goal_owner)
         break
       end
     end
@@ -332,11 +398,12 @@ end
 
 # This creates a message containing the savings goal (if there is one) to be written
 # to the output file later
-def savings_goal_message_func(savings_goal_calculations, savings_goal)
+def savings_goal_message_func(savings_goal_calculations, savings_goal, savings_goal_owner)
   savings_goal_message = <<~SAVINGSMESSAGE
 
 
   ~ SAVINGS GOAL ~
+  #{savings_goal_owner}'s Goal
   If you save $#{sprintf('%.2f', savings_goal_calculations[0])} each month,
   you will reach your #{savings_goal[:name]} goal in #{sprintf('%.0f', savings_goal_calculations[1])} months.
   Stick to it! You'll be glad you did.
@@ -414,8 +481,7 @@ Here is a break down of your monthly budget:
   * You can afford $#{sprintf('%.2f', budget_categories[:gas])} in gas.
   * You can afford $#{sprintf('%.2f', budget_categories[:groceries])} in groceries and food.
 
-This leaves roughly $#{sprintf('%.2f', group_leftovers)} left over
-for spending and saving.
+This leaves roughly $#{sprintf('%.2f', group_leftovers)} left over for spending and saving.
 Keep in mind that these estimates are based on averages,
 and provide room for flexibility.
 Also remember to try and stay under budget so that
