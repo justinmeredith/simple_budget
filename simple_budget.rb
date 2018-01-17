ca2a2fa295baa5a8e44025a21615c2a36726cfe9
Justin Meredith <jmeredith.4444@gmail.com>
2018-01-17 15:08:58 -0600

    Adds the savings feature, tidies up the code, and formats the messages to the user

--- simple_budget.rb
+++ simple_budget.rb
@@ -2,16 +2,18 @@
 
 #Simple_Budget.rb
 
-This program calculates what someone/s make per month, per year,
-and what they can afford to pay monthly for a living space
+This program calculates what someone/s make per month and year,
+and generates a budget based on this information
 
 =end
 
 # This clears the terminal screen.
 Gem.win_platform? ? (system "cls") : (system "clear")
 
+
+# ~   ~   ~   ~   ~   ~   CLASSES    ~   ~   ~   ~   ~   ~ #
 class Member
-  attr_reader :name, :monthly_income, :annual_income, :living, :bills, :gas, :groceries, :leftovers
+  attr_reader :name, :monthly_income, :annual_income, :living, :bills, :gas, :groceries, :leftovers, :savings_goal
 
   def initialize(name, hourly_pay, hours_worked_weekly)
     @name = name
@@ -20,6 +22,7 @@ class Member
     monthly_income_func(@hourly_pay, @hours_worked_weekly)
     annual_income_func(@monthly_income)
     budget(@monthly_income)
+    @savings_goal = String.new
   end
 
   def monthly_income_func(hourly_pay, hours_worked_weekly)
@@ -36,6 +39,29 @@ class Member
     @gas = monthly_income * 0.2
     @groceries = monthly_income * 0.18
     @leftovers = monthly_income - (@living + @bills + @gas + @groceries)
+  end
+
+  def savings_goal_func
+    puts "\n#{name}'s Goal"
+    print "   Goal Name (laptop, vacation, etc.): "
+    goal_name = gets.chomp
+    print "   Goal's Cost: $"
+    goal_amount = gets.chomp.to_i
+    puts "\n   *You have roughly $#{sprintf('%.2f', leftovers)} leftover each month. \n   It is recommended that you do not try to save more than this each month.*\n"
+    print "\n   Amount You Plan to Save Monthly: $"
+    savings_per_month = gets.chomp.to_i
+    goal_timeframe = goal_amount.to_f / savings_per_month
+
+    if goal_timeframe > 1
+      month = "months"
+    else
+      month = "month"
+    end
+
+    if @savings_goal == ""
+      @savings_goal = "\n\n#{name}'s Goal"
+    end
+    @savings_goal += "\nIf you save up $#{savings_per_month} each month for your #{goal_name} goal, you will reach this goal in #{sprintf('%.1f', goal_timeframe)} #{month}."
   end
 end
 
@@ -62,6 +88,8 @@ class Budget
   end
 end
 
+
+# ~   ~   ~   ~   ~   ~   MEMBERS    ~   ~   ~   ~   ~   ~ #
 puts "How many people is this budget for?"
 people = gets.chomp.to_i
 members = Hash.new
@@ -81,7 +109,25 @@ end
 
 budget = Budget.new(members)
 
-puts "What would you like to call this budget?"
+
+# ~   ~   ~   ~   ~   ~   SAVINGS    ~   ~   ~   ~   ~   ~ #
+puts "\n\nWould you like to create a savings goal? (y/n)"
+answer = gets.chomp.downcase
+while answer.include?("y")
+  puts "\nWhich member would you like to create the goal for?"
+  members.each do |key, value|
+    puts "   * #{key}"
+  end
+  print "   "
+  answer = gets.chomp
+  members[answer].savings_goal_func
+  puts "Would you like to create another savings goal? (y/n)"
+  answer = gets.chomp.downcase
+end
+
+
+# ~   ~   ~   ~   ~   ~   WRITING    ~   ~   ~   ~   ~   ~ #
+puts "\n\nWhat would you like to call this budget?"
 title = gets.chomp
 
 message = " ~ ~ ~ " + title + " ~ ~ ~\n\n\n"
@@ -109,10 +155,15 @@ MESSAGE
 title += ".txt"
 file = open(title, 'w')
 file.write(message)
+members.each do |key, value|
+  file.write(value.savings_goal)
+end
 file.close
 
 puts "\n\n\nYour budget can be found in the same folder as this program: \n\n#{Dir.pwd}\n\n\n\n"
 
+
+# ~   ~   ~   ~   ~   ~   END    ~   ~   ~   ~   ~   ~ #
 # This pauses the program before clearing the screen after the user presses 'Return'
 $stdin.gets.chomp
 Gem.win_platform? ? (system "cls") : (system "clear")
